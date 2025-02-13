a31864ab6d0dbe3496703802610c1f8d8210b1e0
Kendra Peace <kendra.peace@ontariotechu.net>
2025-02-13 02:24:03 -0500

    Finished Code

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,3 +1,7 @@
 public class HelloWorld {
     //Create a hello world var and print it using the main in MyClass.java
+    String helloWorld = "Hello World!";
+    public String getHelloWorld() {
+        return helloWorld;
+    }
 }
--- MyClass.java
+++ MyClass.java
@@ -2,12 +2,15 @@ public class MyClass {
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
-        
+
         if (a + b == 3) {
             System.out.println("Wow!");
         } else {
             System.out.println("try again");
         }
+        HelloWorld obj = new HelloWorld();
+
+        System.out.println(obj.getHelloWorld());
     }
 }
 
