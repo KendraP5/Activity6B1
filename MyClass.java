1b7d876229fca4b8a4c8da6f805f4182d32675a4
Kendra Peace <kendra.peace@ontariotechu.net>
2025-02-13 02:09:43 -0500

    Wrote MyClass.Java Code

---
+++ MyClass.java
@@ -0,0 +1,13 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+
+        if (a + b == 100) {
+            System.out.println("Wow!");
+        } else {
+            System.out.println("try again");
+        }
+    }
+}
+
