11e76dfe995892c5ca6e63f6b906baee11419418
Kendra Peace <kendra.peace@ontariotechu.net>
2025-02-13 02:13:35 -0500

    Updated if statement to calc correct number

--- MyClass.java
+++ MyClass.java
@@ -2,8 +2,8 @@ public class MyClass {
     public static void main(String[] args) {
         int a = 1;
         int b = 2;
-
-        if (a + b == 100) {
+        
+        if (a + b == 3) {
             System.out.println("Wow!");
         } else {
             System.out.println("try again");
